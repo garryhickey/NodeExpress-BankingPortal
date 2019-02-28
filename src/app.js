0fd91cdd9b8645b4e3f9c076a4eed36efaea2539
Hickey <garry.hickey@sap.com>
2019-02-28 19:38:07 +0000

    Completed part 1

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(src/json/accounts.json);
+const accounts = JSON.parse(accountData);
 app.get('/', (req, res) => res.render('index', {title: 'Index' }));
 
 app.listen(3000, () => console.log('PS project running on port 3000'));
